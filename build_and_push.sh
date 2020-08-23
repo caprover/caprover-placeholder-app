8809d1ac628966105896c880eeb494194ba44870
Kasra Bigdeli <kasra85@gmail.com>
2020-08-23 17:26:12 -0400

    Added Multiarch

--- build_and_push.sh
+++ build_and_push.sh
@@ -5,7 +5,14 @@ APP_PLACEHOLDER_IMAGE_NAME="caprover/caprover-placeholder-app"
 
 echo "Deploying to Docker hub ..."
 
-docker build -t $APP_PLACEHOLDER_IMAGE_NAME .
-docker tag $APP_PLACEHOLDER_IMAGE_NAME $APP_PLACEHOLDER_IMAGE_NAME:latest
-docker push $APP_PLACEHOLDER_IMAGE_NAME:latest
-docker push $APP_PLACEHOLDER_IMAGE_NAME
+# docker build -t $APP_PLACEHOLDER_IMAGE_NAME .
+# docker tag $APP_PLACEHOLDER_IMAGE_NAME $APP_PLACEHOLDER_IMAGE_NAME:latest
+# docker push $APP_PLACEHOLDER_IMAGE_NAME:latest
+# docker push $APP_PLACEHOLDER_IMAGE_NAME
+
+export DOCKER_CLI_EXPERIMENTAL=enabled
+docker buildx ls
+docker buildx create --name mybuilder
+docker buildx use mybuilder
+
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t $APP_PLACEHOLDER_IMAGE_NAME:multiarch --push .
