fb8f8efb9b06e8235141162604d8133d5233af88
Kasra Bigdeli <kasra85@gmail.com>
2020-08-23 17:10:20 -0400

    Reformatted

--- deploy-placeholder-to-dockerhub.sh
+++ deploy-placeholder-to-dockerhub.sh
@@ -5,9 +5,7 @@ APP_PLACEHOLDER_IMAGE_NAME="caprover/caprover-placeholder-app"
 
 echo "Deploying to Docker hub ..."
 
- docker build -t $APP_PLACEHOLDER_IMAGE_NAME .
-
- docker tag $APP_PLACEHOLDER_IMAGE_NAME $APP_PLACEHOLDER_IMAGE_NAME:latest
-
- docker push $APP_PLACEHOLDER_IMAGE_NAME:latest
- docker push $APP_PLACEHOLDER_IMAGE_NAME
+docker build -t $APP_PLACEHOLDER_IMAGE_NAME .
+docker tag $APP_PLACEHOLDER_IMAGE_NAME $APP_PLACEHOLDER_IMAGE_NAME:latest
+docker push $APP_PLACEHOLDER_IMAGE_NAME:latest
+docker push $APP_PLACEHOLDER_IMAGE_NAME
